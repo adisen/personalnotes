8ac1a763656d4903277d82461ea121262e501af6
Israel Adetunji <israeleadetunji@gmail.com>
2023-03-30 22:38:04 +0100

    make register view send a verification email and redirect to verify_email_view

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:personalnotes/constants/routes.dart';
 import 'package:personalnotes/utilities/show_error_dialog.dart';
 
@@ -70,11 +68,13 @@ class _RegisterViewState extends State<RegisterView> {
                   final password = _password.text;
 
                   try {
-                    final userCredential = await FirebaseAuth.instance
-                        .createUserWithEmailAndPassword(
-                            email: email, password: password);
-
-                    (userCredential);
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    final user = FirebaseAuth.instance.currentUser;
+                    await user?.sendEmailVerification();
+                    Navigator.of(context).pushNamed(verifyEmailRoute);
                   } on FirebaseAuthException catch (e) {
                     if (e.code == 'weak-password') {
                       await showErrorDialog(
