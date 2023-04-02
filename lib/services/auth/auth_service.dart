39a14c48901d68b8ddb6dce8b94ed98e2ca9599a
Israel Adetunji <israeleadetunji@gmail.com>
2023-04-02 23:09:08 +0100

    abstract firebase provider,service and functionalities

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,39 @@
+import "auth_provider.dart";
+import "auth_user.dart";
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+  const AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) {
+    return provider.createUser(email: email, password: password);
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser {
+    return provider.currentUser;
+  }
+
+  @override
+  Future<AuthUser> login({
+    required String email,
+    required String password,
+  }) {
+    return provider.login(email: email, password: password);
+  }
+
+  @override
+  Future<void> logout() {
+    return provider.logout();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    return provider.sendEmailVerification();
+  }
+}
